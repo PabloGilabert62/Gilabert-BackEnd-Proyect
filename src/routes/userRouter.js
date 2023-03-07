b663d2fc218c8e699987516c6f517192ba3cf143
Pablo Gilabert <gilabertpablo62@gmail.com>
2023-03-07 18:22:29 -0300

    error en post

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -12,4 +12,22 @@ userRouter.get("/", async (req, res) => {
     }
 })
 
+userRouter.post("/", async (req, res) => {
+    
+    try {
+        const {name, lastname, email, age} = req.body
+
+        console.log(name, lastname, email, age)
+        const resultado = await userModel.create({
+            name,
+            lastname,
+            email,
+            age
+        })
+        res.send({result: "succes", resultado : resultado})
+    } catch (error) {
+        res.send("Error in users: " + error.message)
+    }
+})
+
 export default userRouter
